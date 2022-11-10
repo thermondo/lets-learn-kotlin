c06ce202368d6ae77204c11ea3dd7098f5816ac4
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-10 17:09:24 +0100

    Solved Exercises

--- basics/src/main/kotlin/de/thermondo/basics/Basics15.kt
+++ basics/src/main/kotlin/de/thermondo/basics/Basics15.kt
@@ -5,6 +5,9 @@ import java.lang.Exception
 /**
  *
  * try as expression
+ * It works more like if-else in java and when in kotlin.
+ *
+ * If the condition is true it will execute the try block if not it will return the value from catch.
  */
 
 fun main() {
---
+++ basics/src/main/kotlin/de/thermondo/basics/Basics16.kt
@@ -0,0 +1,18 @@
+package de.thermondo.basics
+
+/**
+ * 3.1 Kotlin Collection
+ *
+ * Let us create an example using kotlin collections that returns the maximum number from the set and
+ * create a list of strings and return the last element from the list.
+ *
+ */
+
+fun main() {
+    val num = setOf<Int>(1, 14, 2, 52)
+    println(num.max())
+
+    val list = listOf<String>("Initial", "Middle", "Last")
+    println(list.last())
+
+}
