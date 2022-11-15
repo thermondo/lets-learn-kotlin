2117e9b44b79390f6faba3dae7883efdbd075143
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-15 12:04:29 +0100

    basics 5 and test

---
+++ basics/src/main/kotlin/de/thermondo/basics/Basics5.kt
@@ -0,0 +1,9 @@
+package de.thermondo.basics
+
+/**
+ * Create a class named Basics5
+ *
+ * Define member variables:
+ * name: String
+ * age: Int
+ */
---
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics5Solution.kt
@@ -0,0 +1,15 @@
+package de.thermondo.solutions.basics
+
+/**
+ * Create a class named Basics5
+ *
+ * Define member variables:
+ * name: String
+ * age: Int
+ */
+
+@Suppress("MagicNumber")
+class Basics5Solution {
+    val name: String = "Kotlin"
+    val age: Int = 26
+}
