5e8396d8c0a2677ee356d9c54c77e5d5a589eb0a
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-16 17:28:06 +0100

    Changed made

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics08Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics08Solution.kt
@@ -4,24 +4,24 @@ package de.thermondo.solutions.basics
  * Define an enum class named Basics8Solution with properties and print the rgb value for a specific color.
  */
 
-enum class Basics8Solution(val r:Int, val g:Int, val b:Int) {
+enum class Basics08Solution(val r:Int, val g:Int, val b:Int) {
     RED(255,0,0),
     ORANGE(255,265,0),
     YELLOW(255,255,0),
     GREEN(0,255,0),
     BLUE(0,0,255),
     INDIGO(75,0,130),
-    VOILET(238,130,238);
+    VIOLET(238,130,238);
 
     fun rgb() = (r * 256 + g) * 256+ b
 }
 
 fun main() {
-    println(Basics8Solution.RED.rgb())
-    println(Basics8Solution.ORANGE.rgb())
-    println(Basics8Solution.YELLOW.rgb())
-    println(Basics8Solution.GREEN.rgb())
-    println(Basics8Solution.BLUE.rgb())
-    println(Basics8Solution.INDIGO.rgb())
-    println(Basics8Solution.VOILET.rgb())
+    println(Basics08Solution.RED.rgb())
+    println(Basics08Solution.ORANGE.rgb())
+    println(Basics08Solution.YELLOW.rgb())
+    println(Basics08Solution.GREEN.rgb())
+    println(Basics08Solution.BLUE.rgb())
+    println(Basics08Solution.INDIGO.rgb())
+    println(Basics08Solution.VIOLET.rgb())
 }
--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics08SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics08SolutionTest.kt
@@ -5,16 +5,14 @@ import kotlin.test.assertEquals
 
 class Basics08SolutionTest{
 
-    private val colors = Basics8Solution.values()
-
     @Test
-    fun `Validate the rgb value of a color`(){
-        assertEquals(16711680, Basics8Solution.RED.rgb())
-        assertEquals(16779520, Basics8Solution.ORANGE.rgb())
-        assertEquals(16776960, Basics8Solution.YELLOW.rgb())
-        assertEquals(65280, Basics8Solution.GREEN.rgb())
-        assertEquals(255, Basics8Solution.BLUE.rgb())
-        assertEquals(4915330, Basics8Solution.INDIGO.rgb())
-        assertEquals(15631086, Basics8Solution.VOILET.rgb())
+    fun `Validate the rgb value of the color`(){
+        assertEquals(16711680, Basics08Solution.RED.rgb())
+        assertEquals(16779520, Basics08Solution.ORANGE.rgb())
+        assertEquals(16776960, Basics08Solution.YELLOW.rgb())
+        assertEquals(65280, Basics08Solution.GREEN.rgb())
+        assertEquals(255, Basics08Solution.BLUE.rgb())
+        assertEquals(4915330, Basics08Solution.INDIGO.rgb())
+        assertEquals(15631086, Basics08Solution.VIOLET.rgb())
     }
 }
