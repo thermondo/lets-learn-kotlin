d2de7895ddfb8fa47f511a0975b42b94f520c62c
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-16 15:03:26 +0100

    asics09asics09SolutionBasics09 Solution

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics09Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics09Solution.kt
@@ -5,37 +5,37 @@ package de.thermondo.solutions.basics
  *  Let's define an enum class named Basics9Solution using "when" expression to print number for each month of the year.
 */
 
-enum class Basics9Solutions() {
-    January,
-    February,
-    March,
-    April,
-    May,
-    June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December
+enum class Basics09Solutions() {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
 }
 
 fun main() {
-    val year = Basics9Solutions.May
+    val year = Basics09Solutions.MAY
     when(year){ /**Here, "year" is an argument. The "when" expression matches its arguments against all branches
     in order until some branch condition is satisfied.*/
-        Basics9Solutions.January -> println("1")
-        Basics9Solutions.February -> println("2")
-        Basics9Solutions.March -> println("3")
-        Basics9Solutions.April -> println("4")
-        Basics9Solutions.May -> println("5")
-        Basics9Solutions.June -> println("6")
-        Basics9Solutions.July -> println("7")
-        Basics9Solutions.August -> println("8")
-        Basics9Solutions.September -> println("9")
-        Basics9Solutions.October -> println("10")
-        Basics9Solutions.November -> println("11")
-        Basics9Solutions.December -> println("12")
+        Basics09Solutions.JANUARY -> println("1")
+        Basics09Solutions.FEBRUARY -> println("2")
+        Basics09Solutions.MARCH -> println("3")
+        Basics09Solutions.APRIL -> println("4")
+        Basics09Solutions.MAY-> println(5)
+        Basics09Solutions.JUNE -> println("6")
+        Basics09Solutions.JULY -> println("7")
+        Basics09Solutions.AUGUST -> println("8")
+        Basics09Solutions.SEPTEMBER -> println("9")
+        Basics09Solutions.OCTOBER -> println("10")
+        Basics09Solutions.NOVEMBER -> println("11")
+        Basics09Solutions.DECEMBER -> println("12")
     }
     //println(Year.May)
 }
--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics09SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics09SolutionTest.kt
@@ -1,3 +1,14 @@
 package de.thermondo.solutions.basics
 
-class Basics09SolutionTest
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Basics09SolutionTest {
+
+    private val month = Basics09Solutions.values()
+
+    @Test
+    fun `Validate the corresponding month number`(){
+        assertEquals(5, Basics09Solutions.MAY)
+    }
+}
