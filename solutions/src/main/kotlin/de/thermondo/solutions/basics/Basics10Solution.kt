b58f260db81a57af3dbeb3cc34d553637de83876
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-18 11:36:54 +0100

    Edited Basics10

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics10Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics10Solution.kt
@@ -1,10 +1,16 @@
 package de.thermondo.solutions.basics
-
+import com.sun.tools.javac.Main
 /**
  * Print the table of 2 using while loop.
  */
-
 fun main() {
+    loopWhile()
+}
+/*fun main() {
+    println(whileLoop(index = 1))
+}*/
+
+fun loopWhile() {
     var num = 2
     var index = 1
     while (index <= 10)
@@ -12,6 +18,16 @@ fun main() {
         println(num*index)
         index++
     }
-
-    print("Current Index : "+index)
+    //print("Current Index : "+index)
 }
+
+/*fun whileLoop(index:Int) :Int{
+    var num = 2
+    //var index = 1
+    while (index <= 10)
+    {
+        println(num*index)
+        index++
+    }
+    //print("Current Index : "+index)
+}*/
--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics10SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics10SolutionTest.kt
@@ -1,5 +1,18 @@
 package de.thermondo.solutions.basics
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.reflect.KMutableProperty
 
-class Basics10SolutionTest
+class Basics10Solution {
+
+    @Test
+    fun `Validate if the index is within the range or not`() {
+        val i =Basics10Solution()
+        when (i in 2..20 && i % 2 == 0) {
+            assertEquals(4, loopWhile())
+        }
+    }
+}
+
+
