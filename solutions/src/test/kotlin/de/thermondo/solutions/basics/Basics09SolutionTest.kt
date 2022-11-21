6d4d5007aff8a8bd1bd96bd46979ace86be7af43
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-21 15:05:57 +0100

    Basics09Test Edited

--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics09SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics09SolutionTest.kt
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test
 import de.thermondo.test.utils.BasePrintlnTest
 import kotlin.test.assertEquals
 
-<<<<<<< Updated upstream
-
 class Basics09SolutionTest: BasePrintlnTest() {
-=======
-class Basics09SolutionTest {
->>>>>>> Stashed changes
-    @Test
-    fun `Validate the corresponding month number`(){
-        assertEquals(12, enumMonths())
+    class Basics09SolutionTest {
+
+        @Test
+        fun `Validate the corresponding month number`() {
+            assertEquals(12, enumMonths())
+        }
     }
 }
