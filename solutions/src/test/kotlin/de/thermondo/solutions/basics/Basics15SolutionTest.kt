c927edaad20434cda1d47792cb3d28f115003221
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-22 14:33:10 +0100

    Basics15 solution and test

--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics15SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics15SolutionTest.kt
@@ -1,5 +1,20 @@
 package de.thermondo.solutions.basics
 
+import de.thermondo.test.utils.BasePrintlnTest
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class Basics15SolutionTest
+class Basics15SolutionTest{
+
+    @Test
+    fun `Validate the integers`(){
+        assertEquals(456, tryCatch())
+    }
+/*
+    @Test
+    fun `Validate the exception`(){
+        assertEquals(0, tryCatch())
+    }
+*/
+
+}
