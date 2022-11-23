2e336398fb04acc60a6f8744d3b3d30f29e5caaa
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-23 14:10:32 +0100

    Basics06 Edited

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics06Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics06Solution.kt
@@ -9,10 +9,6 @@ package de.thermondo.solutions.basics
  * and return the data of the two people while writing unit tests.
  */
 class Basics06Solution(name: String, age: Int) {
-    //Here, name and age are the constructors.
-    // Because we call it when we create an instance of the class.(line 10 & 14).
-    // A constructor constructs an object and prepares to use.
     val name = name // The variables that are part of the class are called as properties.
     var age = age
 }
-
