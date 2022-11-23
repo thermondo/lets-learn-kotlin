2b032cffdfdb931650ef6ad9a22d3f4ea86929f9
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-23 13:58:14 +0100

    Basics06 solution and test

---
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics06SolutionTest.kt
@@ -0,0 +1,29 @@
+package de.thermondo.solutions.basics
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Basics06SolutionTest{
+
+    private val user1 = Basics06Solution(name = "Tom", age = 32)
+    private val user2 = Basics06Solution(name = "Venelope", age = 21)
+
+    @Test
+    fun `Validate first user's name`() {
+        assertEquals(expected = "Tom", actual = user1.name)
+    }
+
+    @Test
+    fun `Validate first user's age`() {
+        assertEquals(expected = 32, actual = user1.age)
+    }
+    @Test
+    fun `Validate second user's name`() {
+        assertEquals(expected = "Venelope", actual = user2.name)
+    }
+
+    @Test
+    fun `Validate second user's age`() {
+        assertEquals(expected = 21, actual = user2.age)
+    }
+}
