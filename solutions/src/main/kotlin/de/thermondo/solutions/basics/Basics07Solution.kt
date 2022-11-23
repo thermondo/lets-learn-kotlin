bdef59898e127d34e3159015334a4c95485d538a
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-23 17:12:40 +0100

    Basics07 solution and test

---
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics07Solution.kt
@@ -0,0 +1,23 @@
+package de.thermondo.solutions.basics
+
+/**
+ * Enumeration is a named list of constants.
+ * In Kotlin, an enum has its own specialized type, indicating that something has a number of possible values.
+ * and it's defined by using enum keyword before a class.
+ *
+ * Define an enum class named Basics6Solution that prints the number of days of the week to the console.
+ */
+
+enum class Basics7Solution {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
+fun main() {
+    Basics7Solution.values().forEach { println(it) }
+}
