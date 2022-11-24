c8ede637ec1a5dc12d942a077db8fdeeb10e724c
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-24 14:18:29 +0100

    fixed lint

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics08Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics08Solution.kt
@@ -3,16 +3,16 @@ package de.thermondo.solutions.basics
 /**
  * Define an enum class named Basics8Solution with properties and print the rgb value for a specific color.
  */
+@Suppress("MagicNumber")
+enum class Basics08Solution(val r: Int, val g: Int, val b: Int) {
 
-enum class Basics08Solution(val r:Int, val g:Int, val b:Int) {
-    
-    RED(255,0,0),
-    ORANGE(255,265,0),
-    YELLOW(255,255,0),
-    GREEN(0,255,0),
-    BLUE(0,0,255),
-    INDIGO(75,0,130),
-    VIOLET(238,130,238);
+    RED(255, 0, 0),
+    ORANGE(255, 265, 0),
+    YELLOW(255, 255, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75, 0, 130),
+    VIOLET(238, 130, 238);
 
     val x = 256
     fun rgb() = (r * x + g) * x + b
--- solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics08SolutionTest.kt
+++ solutions/src/test/kotlin/de/thermondo/solutions/basics/Basics08SolutionTest.kt
@@ -3,10 +3,10 @@ package de.thermondo.solutions.basics
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class Basics08SolutionTest{
+class Basics08SolutionTest {
 
     @Test
-    fun `Validate the rgb value of the color`(){
+    fun `Validate the rgb value of the color`() {
         assertEquals(16711680, Basics08Solution.RED.rgb())
         assertEquals(16779520, Basics08Solution.ORANGE.rgb())
         assertEquals(16776960, Basics08Solution.YELLOW.rgb())
