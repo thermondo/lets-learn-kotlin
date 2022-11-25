b30e1eb0b25a1a64e2363410758034e95975dbbf
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-25 14:33:20 +0100

    Not yet solved

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics13Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics13Solution.kt
@@ -6,17 +6,18 @@ package de.thermondo.solutions.basics
 fun main() {
     tryCatchException()
 }
+@Suppress("MagicNumber")
 fun tryCatchException(): String {
     var arr: Array<Int> = arrayOf(1,2,3)
     try {
         println(arr[10])
     }
 
-    catch (e: Exception) {
-       var x =  println("Please check the array index")
+    catch (exc: ArrayIndexOutOfBoundsException) {
+        println("Please check the array index")
     }
     finally {
-        var y =println("I am finally and I execute no matter what")
+        println("I am finally and I execute no matter what")
        // return x + y
     }
   return "Please check the array index\nI am finally and I execute no matter what"
