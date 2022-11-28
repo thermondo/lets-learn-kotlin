f45eb4b4bbb964428a296714745ac9a4b9937f54
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-28 14:05:12 +0100

    Basics16 Solution and test

---
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics16Solution.kt
@@ -0,0 +1,27 @@
+package de.thermondo.solutions.basics
+
+/**
+ * 3.1 Kotlin Collection
+ *
+ * Let us create an example using kotlin collections that returns the maximum number from the set and
+ * create a list of strings and return the last element from the list.
+ *
+ */
+
+fun main() {
+    intCollection()
+    stringCollection()
+}
+
+@Suppress("MagicNumber")
+fun intCollection(): Int {
+    val num = setOf<Int>(1, 14, 2, 52)
+    println(num.max())
+    return num.max()
+}
+
+fun stringCollection(): String {
+    val list = listOf<String>("Initial", "Middle", "Last")
+    println(list.last())
+    return list.last()
+}
