ca7482fdddd6ba806f43068caafb13ed8b30369b
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-29 14:50:21 +0100

    edited for codeCov

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics18Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics18Solution.kt
@@ -10,10 +10,6 @@ package de.thermondo.solutions.basics
 }
 */
 
-fun main() {
-    namedArgs(age = 32)
-}
-
 fun namedArgs(name: String = "xyz", age: Int = 22): Any {
     println("My name is $name and I am $age years old")
     return "My name is $name and I am $age years old"
