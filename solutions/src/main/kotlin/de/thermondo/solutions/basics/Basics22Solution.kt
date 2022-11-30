0b9b6738921ffd92521cdd580017fbdbb0faa9ef
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-30 17:24:19 +0100

    edited comments

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics22Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics22Solution.kt
@@ -3,7 +3,8 @@ package de.thermondo.solutions.basics
 /* Create an extension function with teh library class String and print the character at the position number 2.
 */
 
-fun String.findChar(index: Int): Char = this[index]
+fun String.findChar(index: Int): Char = this[index] // 'this' represents the current object for example, this means
+                                                    // String here.
 /*
 fun main() {
     println("kotlin".findChar(2))
