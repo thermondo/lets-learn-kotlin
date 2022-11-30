ed95a854982b8e2d80f08fdea5dac71f246a32cf
Sonal Dhage <sonal.dhage@thermondo.de>
2022-11-30 16:57:16 +0100

    Basics21 fixed for codeCov

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics21Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics21Solution.kt
@@ -2,11 +2,12 @@ package de.thermondo.solutions.basics
 
 /* Create an extension function to swap the numbers in a mutable list. (Using this keyword) */
 
+/*
 @Suppress("MagicNumber")
 fun main() {
     val list = mutableListOf(12, 34, 45, 32)
     println(list.swap(0, 2))
-}
+}*/
 
 fun MutableList<Int>?.swap(first: Int, second: Int): Any {
     if (this == null) {
