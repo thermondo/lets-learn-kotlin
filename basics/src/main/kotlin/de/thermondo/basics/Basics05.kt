b9f57ddcf263b1acce47f2c7d9dcb07395713719
Sonal Dhage <sonal.dhage@thermondo.de>
2022-12-05 14:44:10 +0100

    Added description about the class

---
+++ basics/src/main/kotlin/de/thermondo/basics/Basics05.kt
@@ -0,0 +1,12 @@
+package de.thermondo.basics
+
+/**
+ * Use the `class` keyword to define a class.
+ * Class naming is done in upper camel case.
+ *
+ * Create a class named Basics05
+ *
+ * Define member variables:
+ * name: String
+ * age: Int
+ */
---
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics05Solution.kt
@@ -0,0 +1,18 @@
+package de.thermondo.solutions.basics
+
+/**
+ * Use the `class` keyword to define a class.
+ * Class naming is done in upper camel case.
+ *
+ * Create a class named Basics05
+ *
+ * Define member variables:
+ * name: String
+ * age: Int
+ */
+
+@Suppress("MagicNumber")
+class Basics05Solution {
+    val name: String = "Kotlin"
+    val age: Int = 26
+}
