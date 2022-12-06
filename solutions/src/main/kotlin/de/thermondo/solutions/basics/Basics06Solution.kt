a908f3b972be7cdefcd9241b96f2b99b44934953
Sonal Dhage <sonal.dhage@thermondo.de>
2022-12-06 15:51:33 +0100

    changed line no:07 from Basics06 to Basics06Solution

--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics06Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics06Solution.kt
@@ -4,7 +4,7 @@ package de.thermondo.solutions.basics
  * You can pass parameters to classes via the class constructor by first giving the parameter name
  * and then the type, similar to how we create functions with parameters.
  *
- * Let's create a class called Basics06, that accepts the following parameters:
+ * Let's create a class called Basics06Solution, that accepts the following parameters:
  * @param name: String
  * @param age: Int
  *
