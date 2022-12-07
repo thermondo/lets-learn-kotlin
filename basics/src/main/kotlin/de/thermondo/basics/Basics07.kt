ff44f954579d1233d8383c5ec89cbc2aea15bbf2
Sonal Dhage <sonal.dhage@thermondo.de>
2022-12-07 15:50:56 +0100

    Changed the exercise intro

--- basics/src/main/kotlin/de/thermondo/basics/Basics07.kt
+++ basics/src/main/kotlin/de/thermondo/basics/Basics07.kt
@@ -5,5 +5,6 @@ package de.thermondo.basics
  * In Kotlin, an enum has its own specialized type, indicating that something has a number of possible values.
  * and it's defined by using enum keyword before a class.
  *
- * Define an enum class named Basics07Solution that prints the number of days of the week to the console.
+ * Define an enum class named Basics07Solution for the number of days of the week and create a function printEnum to print
+ * the days of the week to the console.
  */
--- solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics07Solution.kt
+++ solutions/src/main/kotlin/de/thermondo/solutions/basics/Basics07Solution.kt
@@ -5,7 +5,8 @@ package de.thermondo.solutions.basics
  * In Kotlin, an enum has its own specialized type, indicating that something has a number of possible values.
  * and it's defined by using enum keyword before a class.
  *
- * Define an enum class named Basics07Solution that prints the number of days of the week to the console.
+ * Define an enum class named Basics07Solution for the number of days of the week and create a function printEnum to print
+ * the days of the week to the console.
  */
 
 enum class Basics07Solution {
